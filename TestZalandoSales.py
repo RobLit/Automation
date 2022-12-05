b47426d432082bf4569bcef7ea62608e72ac53ce
RobLit <robert.litwin91@gmail.com>
2022-12-05 12:28:24 +0100

    MatchaLatte

--- TestZalandoSales.py
+++ TestZalandoSales.py
@@ -12,7 +12,7 @@ def test_oneZalandoCheckout():
 
     driver.get("https://www.zalando.pl/okazje/?sale=true")
 
-    for n in range(2, 5):
+    for n in range(2, 500):
         for i in range(1, 100):
             try:
                 NAME = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]/div/article/div[4]/a/header/div[1]/h3[2]' %i)).text
@@ -31,8 +31,10 @@ def test_oneZalandoCheckout():
             except:
                 continue
             print(NAME, BEFORE, AFTER, SALEPERCENT)
-        driver.get("https://www.zalando.pl/okazje/?sale=true&p=%d" %n)
-
+        try:
+            driver.get("https://www.zalando.pl/okazje/?sale=true&p=%d" %n)
+        except:
+            driver.close()
 
         BEFORE2 = []
         AFTER2 = []
