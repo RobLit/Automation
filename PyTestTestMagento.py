8e445e3b34837393a6ac363f0c3c993128a650bb
RobLit <robert.litwin91@gmail.com>
2022-12-07 20:53:30 +0100

    MatchaLatte

--- PyTestTestMagento.py
+++ PyTestTestMagento.py
@@ -13,8 +13,10 @@ def test_setup():
     global list2
     list2 = ["Tank", "Tee", "Insulated", "Heavy Duty", "Soft Shell", "¼ zip", "Lightweight", "Hooded", "Hard Shell",
              "Windbreaker", "Full Zip", "Jacket", "Rain Coat"]
+    global list3
+    list3 = ["Base Layer", "Compression", "Tights", "Workout Pants", "Leggings", "Track Pants", "Sweatpants"]
 
-def test_one():
+def test_one_BottomsWomen():
     driver.get("https://magento.softwaretestingboard.com/women/bottoms-women.html?product_list_limit=36")
     for n in range(1, 26):
         try:
@@ -36,10 +38,10 @@ def test_one():
         elements_in_list = [elem for elem in NAME if elem in list]
         for got_element in NAME:
             if not got_element in list:
-                print(f"Retrieved element {got_element} not in list {list} at ", ID)
-                raise AssertionError()
+                raise AssertionError(f"Retrieved element {got_element} not in list {list} at ", ID)
         driver.back()
-def test_two():
+
+def test_two_TopsWomen():
     driver.get("https://magento.softwaretestingboard.com/women/tops-women/hoodies-and-sweatshirts-women.html")
     for n in range(1, 12):
         try:
@@ -61,10 +63,10 @@ def test_two():
         elements_in_list1 = [elem for elem in NAME if elem in list1]
         for got_element in NAME:
             if not got_element in list1:
-                print(f"Retrieved element {got_element} not in list1 {list1} at ", ID)
-                raise AssertionError()
+                raise AssertionError(f"Retrieved element {got_element} not in list1 {list1} at ", ID)
         driver.back()
-def test_three():
+
+def test_three_TopsMen():
     driver.get("https://magento.softwaretestingboard.com/men/tops-men.html?product_list_limit=36")
     for n in range(1, 36):
         try:
@@ -86,9 +88,9 @@ def test_three():
         elements_in_list1 = [elem for elem in NAME if elem in list1]
         for got_element in NAME:
             if not got_element in list2:
-                print(f"Retrieved element {got_element} not in list2 {list1} at ", ID)
-                raise AssertionError
+                raise AssertionError(f"Retrieved element {got_element} not in list {list2} at ", ID)
         driver.back()
+
     driver.find_element(By.CLASS_NAME, "page").click()
     for n in range(1, 12):
         try:
@@ -110,13 +112,33 @@ def test_three():
         elements_in_list2 = [elem for elem in NAME if elem in list2]
         for got_element in NAME:
             if not got_element in list2:
-                print(f"Retrieved element {got_element} not in list2 {list2} at ", ID)
-                raise AssertionError
+                raise AssertionError(f"Retrieved element {got_element} not in list {list2} at ", ID)
         driver.back()
 
+def test_four_BottomsMen():
+    driver.get("https://magento.softwaretestingboard.com/men/bottoms-men.html?product_list_limit=36")
+    for n in range(1, 24):
+        try:
+            driver.find_element(By.XPATH, ('//*[@id="maincontent"]/div[3]/div[1]/div[3]/ol/li[%d]/div/a/span/span/img' % n)).click()
+        except:
+            AssertionError()
+        try:
+            driver.find_element(By.LINK_TEXT, "More Information").click()
+        except:
+            AssertionError()
+        try:
+            NAME = driver.find_element(By.XPATH, '//*[@data-th="Style"]').text.split(', ')
+        except:
+            AssertionError()
+        try:
+            ID = driver.find_element(By.CLASS_NAME, "value").text
+        except:
+            AssertionError()
+        elements_in_list = [elem for elem in NAME if elem in list3]
+        for got_element in NAME:
+            if not got_element in list3:
+                raise AssertionError(f"Retrieved element {got_element} not in list {list3} at ", ID)
+        driver.back()
 def test_teardown():
     driver.close()
 
-#wpisać komendy błędów w AssertionError'ach
-#dokończyć testy
-#sprawdzić "sale"
