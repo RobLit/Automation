79d4a299bbf12ce50b4e2f60c08bc62f7f2404d8
RobLit <robert.litwin91@gmail.com>
2022-12-18 13:09:32 +0100

    MatchaLatte

--- ZalandoSortingTest1.py
+++ ZalandoSortingTest1.py
@@ -1,53 +1,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common import NoSuchElementException
+import pytest
+
 
 def test_setup():
     global driver
     driver = webdriver.Edge(executable_path="GitTest/drivers/msedgedriver.exe")
 
-def test_ZalandoSortingPriceLow2High():
+def test_oneZalandoASC():
     driver.get("https://www.zalando.pl/dzieci-akcesoria/?order=price&dir=asc")
-    for a in range(2, 67):
-        for i in range(1, 100):
+    for n in range (2, 65):
+        for i in range (1, 100):
             try:
-                NAME = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]' % i)).text
-                PRICE = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]/div/article/div[5]/a/header/section/p' % i)).text
-                PRICE3 = PRICE.removeprefix("od").replace(" ", "").removesuffix("zł")
-                PRICE2 = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d+1]/div/article/div[5]/a/header/section/p' % i)).text
-                PRICE4 = PRICE2.removeprefix("od").replace(" ", "").removesuffix("zł")
-            except:
-                NAME = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]' % i)).text
-                PRICE = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]/div/article/div[4]/a/header/section/p' % i)).text
-                PRICE3 = PRICE.removeprefix("od").replace(" ", "").removesuffix("zł")
-                PRICE2 = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d+1]/div/article/div[4]/a/header/section/p' % i)).text
-                PRICE4 = PRICE2.removeprefix("od").replace(" ", "").removesuffix("zł")
-            finally:
-                if PRICE3 > PRICE4:
-                    print("There is problem with:" + NAME)
+                NAME = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]' %i)).text
+                PRICE = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]/div/article/div[4]/a/header/section/p' %i)).text
+                PRICE2 = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d+1]/div/article/div[4]/a/header/section/p' %i)).text
+                PRICE3 = float(PRICE.removeprefix("od").replace(" ", "").replace(",", ".").removesuffix("zł"))
+                PRICE4 = float(PRICE2.removeprefix("od").replace(" ", "").replace(",", ".").removesuffix("zł"))
+                if PRICE4 < PRICE3:
+                    pytest.fail("There is problem with:" + " " + NAME)
+            except NoSuchElementException:
                 continue
-        driver.get("https://www.zalando.pl/dzieci-akcesoria/?p=%d&order=price&dir=desc" % a)
-    print("Test done")
+        driver.get("https://www.zalando.pl/dzieci-akcesoria/?p=%d&order=price&dir=asc" %n)
+    print("Testing done")
 
-def test_ZalandoSortingPriceHigh2Low():
+def test_twoZalandoDESC():
     driver.get("https://www.zalando.pl/dzieci-akcesoria/?order=price&dir=desc")
-    for a in range(2, 67):
+    for n in range(2, 65):
         for i in range(1, 100):
             try:
                 NAME = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]' % i)).text
-                PRICE = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]/div/article/div[5]/a/header/section/p' % i)).text
-                PRICE3 = PRICE.removeprefix("od").replace(" ", "").removesuffix("zł")
-                PRICE2 = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d+1]/div/article/div[5]/a/header/section/p' % i)).text
-                PRICE4 = PRICE2.removeprefix("od").replace(" ", "").removesuffix("zł")
-            except:
-                NAME = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]' % i)).text
                 PRICE = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d]/div/article/div[4]/a/header/section/p' % i)).text
-                PRICE3 = PRICE.removeprefix("od").replace(" ", "").removesuffix("zł")
                 PRICE2 = driver.find_element(By.XPATH, ('//*[@id="main-content"]/div/div/div[7]/div/div[2]/div[2]/div[2]/div[%d+1]/div/article/div[4]/a/header/section/p' % i)).text
-                PRICE4 = PRICE2.removeprefix("od").replace(" ", "").removesuffix("zł")
-            finally:
-                if PRICE3 < PRICE4:
-                    print("There is problem with:" + NAME)
+                PRICE3 = float(PRICE.removeprefix("od").replace(" ", "").replace(",", ".").removesuffix("zł"))
+                PRICE4 = float(PRICE2.removeprefix("od").replace(" ", "").replace(",", ".").removesuffix("zł"))
+                if PRICE4 > PRICE3:
+                    pytest.fail("There is problem with:" + " " + NAME)
+            except NoSuchElementException:
                 continue
-        driver.get("https://www.zalando.pl/dzieci-akcesoria/?p=%d&order=price&dir=desc" % a)
-    print("Test done")
-
+        driver.get("https://www.zalando.pl/dzieci-akcesoria/?p=%d&order=price&dir=desc" % n)
+    print("Testing done")
